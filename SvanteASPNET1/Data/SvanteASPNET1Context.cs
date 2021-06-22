0edf1f8d0e39de4d8947b796a130ca3a0fbee5b7
svante1987 <svantepalsson1987@gmail.com>
2021-06-23 01:50:07 +0200

    lägger till seed metod i min context fil.

--- SvanteASPNET1/Data/SvanteASPNET1Context.cs
+++ SvanteASPNET1/Data/SvanteASPNET1Context.cs
@@ -9,11 +9,35 @@ namespace SvanteASPNET1.Data
 {
     public class SvanteASPNET1Context : DbContext
     {
-        public SvanteASPNET1Context (DbContextOptions<SvanteASPNET1Context> options)
+        public SvanteASPNET1Context(DbContextOptions<SvanteASPNET1Context> options)
             : base(options)
         {
         }
 
-        public DbSet<SvanteASPNET1.Models.Event> Event { get; set; }
+        public DbSet<Event> Event { get; set; }
+
+        public void Seed()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+
+            Attendee[] attendees = new Attendee[] {
+                new Attendee()
+                {
+                    Name = "Svante Vantesson",
+                    PhoneNumber = "+46 123 456 71",
+                    Email = "svantethevantesson@varsomhelst.se",
+                },
+            };
+
+            Organizer[] organizers = new Organizer[] {
+                new Organizer(){
+                    Name = "RockMaskinen",
+                    Email = "rock@maskin.se",
+                    PhoneNumber = "46 666 666 66",
+                },
+            };
+        }
     }
 }
+
