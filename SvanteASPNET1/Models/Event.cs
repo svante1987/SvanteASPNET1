58d2d5d9ce59d47afb89632e81566269feb9559a
svante1987 <svantepalsson1987@gmail.com>
2021-06-22 13:46:46 +0200

    gjort mina två andra modeller för event och organazier

--- SvanteASPNET1/Models/Event.cs
+++ SvanteASPNET1/Models/Event.cs
@@ -7,5 +7,14 @@ namespace SvanteASPNET1.Models
 {
     public class Event
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Place { get; set; }
+        public string Address { get; set; }
+        public DateTime Date { get; set; }
+        public int SpotsAvailable { get; set; }
+        public List<Attendee> Attendees { get; set; }
+        public Organizer Organizer { get; set; }
     }
 }
--- SvanteASPNET1/Models/Organazier.cs
+++ SvanteASPNET1/Models/Organazier.cs
@@ -5,7 +5,13 @@ using System.Threading.Tasks;
 
 namespace SvanteASPNET1.Models
 {
-    public class Organazier
+    public class Organizer
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public List<Event> Events { get; set; }
     }
 }
