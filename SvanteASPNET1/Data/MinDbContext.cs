fc551b477b6882ee44818bf871d1a8cb81820ede
svante1987 <svantepalsson1987@gmail.com>
2021-06-22 13:57:06 +0200

    Börjat på min DbContext

---
+++ SvanteASPNET1/Data/MinDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SvanteASPNET1.Data
+{
+    public class MinDbContext : DbContext 
+    {
+        public MinDbContext (DbContextOptions<MinDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<SvanteASPNET1.Models.Attendee> Attendee { get; set; }
+
+        public DbSet<SvanteASPNET1.Models.Event> Event { get; set; }
+
+        public DbSet<SvanteASPNET1.Models.Event> Organazier { get; set; }
+    }
+}
