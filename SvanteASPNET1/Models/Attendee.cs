e5df2651cfc8b577fb0d545a903d86f8aad5b580
svante1987 <svantepalsson1987@gmail.com>
2021-06-22 13:45:05 +0200

    gjort min första model för besökare(attendee)

--- SvanteASPNET1/Models/Attendee.cs
+++ SvanteASPNET1/Models/Attendee.cs
@@ -7,5 +7,12 @@ namespace SvanteASPNET1.Models
 {
     public class Attendee
     {
+        
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Email { get; set; }
+            public string PhoneNumber { get; set; }
+            public List<Event> Events { get; set; }
+        
     }
 }
